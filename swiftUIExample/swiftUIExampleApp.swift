6180a9ae5a9eae8b2706d804ef1a625055d6f00a
Vishal Gupta <vishal.gupta@cyberark.com>
2021-07-11 15:28:45 +0530

    Initial Commit

---
+++ swiftUIExample/swiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIExampleApp.swift
+//  swiftUIExample
+//
+//  Created by Vishal Gupta on 11-July-2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
